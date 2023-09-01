6e4eb12cf7638529e16f929a6c2cbd8daab195ab
Yadhir Teoba <yadhirTV@gmail.com>
2023-09-01 10:25:49 -0600

    "changes in mocuk header #4"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  name: string = "Yadhir Teoba"
+  titles: string = "Tecnico en Programacion"
+  aspirations: string = "Poder trabajar y serle de ayuda a la empresa"
+  photo: string = "fake photo"
+  email: string = "yadhirTV@gmail.com"
+  cel: string = "(229) 968 5317"
+  location: string = "Orizaba, Ver."
+  social: string = "@yodoeaofi06"
 }
